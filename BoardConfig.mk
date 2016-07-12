c7cb44e945a188d91679da12c0dd791af1e0b3bd
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2016-07-12 21:37:01 +0200

    klte: move to msm8974-common libinit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,8 +21,8 @@ TARGET_OTA_ASSERT_DEVICE := kltedcmactive,SC-02G
 TARGET_KERNEL_VARIANT_CONFIG := msm8974pro_sec_kactivelte_dcm_defconfig
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_msm
-TARGET_LIBINIT_DEFINES_FILE := device/samsung/kltedcmactive/init/init_klte.cpp
+TARGET_INIT_VENDOR_LIB := libinit_msm8974
+TARGET_LIBINIT_MSM8974_DEFINES_FILE := device/samsung/kltedcmactive/init/init_klte.cpp
 TARGET_UNIFIED_DEVICE := true
 
 # NFC
