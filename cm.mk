6bd5adae6fd3cd1b60787ffe358f6e5487274827
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-11-25 15:15:08 +0900

    klte: Remove enhanced NFC feature

--- cm.mk
+++ cm.mk
@@ -1,7 +1,5 @@
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 $(call inherit-product, device/samsung/kltedcmactive/full_kltedcmactive.mk)
 
 PRODUCT_DEVICE := kltedcmactive
