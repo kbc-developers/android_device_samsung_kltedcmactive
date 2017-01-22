LineageOS/android_device_samsung_klte

klte: Rebrand some of the things
* Update PRODUCT_NAME because the value is user-visable
* Rename cm.mk to lineage.mk because we are touching the file
* Rename cm.dependencies to lineage.dependencies because why not

Change-Id: I5c042e03b30e319bf20209a6a37278e85c9493be

lineage.mk
@@ -3,4 +3,4 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, device/samsung/klte/full_klte.mk)
 
 PRODUCT_DEVICE := klte
-PRODUCT_NAME := cm_klte
+PRODUCT_NAME := lineage_klte

kbc-developers/android_device_samsung_kltedcmactive

lineage.mk
@@ -3,4 +3,4 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, device/samsung/kltedcmactive/full_kltedcmactive.mk)
 
 PRODUCT_DEVICE := kltedcmactive
-PRODUCT_NAME := cm_kltedcmactive
+PRODUCT_NAME := lineage_kltedcmactive
1e0afbd7512e4eff65e662f27befd658ef9e42b9
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2016-12-29 22:03:01 -0700

    klte: Rebrand some of the things

--- lineage.mk
+++ lineage.mk
@@ -3,4 +3,4 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, device/samsung/kltedcmactive/full_kltedcmactive.mk)
 
 PRODUCT_DEVICE := kltedcmactive
-PRODUCT_NAME := cm_kltedcmactive
+PRODUCT_NAME := lineage_kltedcmactive
